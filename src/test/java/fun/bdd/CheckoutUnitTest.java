3881b1ebeec2de06ca564ec6daaa31bc1e5c415f
brian <brian_sjoberg@yahoo.com>
2019-07-11 10:38:56 -0400

    Trying to add test reports from surefire.

--- src/test/java/fun/bdd/CheckoutUnitTest.java
+++ src/test/java/fun/bdd/CheckoutUnitTest.java
@@ -3,10 +3,17 @@ package fun.bdd;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
 public class CheckoutUnitTest {
     @Test
     public void test_should_give_price_based_on_item_name() {
+        Inventory mockInventory = mock(Inventory.class);
+        doReturn(.39).when(mockInventory).getPrice("orange");
 
+        Checkout checkout = new Checkout(mockInventory);
+        checkout.add("orange");
     }
 
     @Test
@@ -32,6 +39,16 @@ public class CheckoutUnitTest {
         checkout.add("banana");
         Assert.assertEquals(1, checkout.items.size());
         Assert.assertEquals(Integer.valueOf(2), checkout.totalQuantity());
+    }
+
+    @Test
+    public void test_should_have_more_quantity_than_items_when_different_items() {
+        Checkout checkout = new Checkout(new Inventory());
+        checkout.add("apple");
+        checkout.add("banana");
+        checkout.add("apple");
+        Assert.assertEquals(2, checkout.items.size());
+        Assert.assertEquals(Integer.valueOf(3), checkout.totalQuantity());
     }
 
     @Test
